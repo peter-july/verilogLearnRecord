b20fb7fac009f014d6486481e2f07cdebbdcc478
peter_ <15800959605@163.com>
2024-05-13 10:19:37 +0800

    add case

--- 20240508_tchkVector/test.sv
+++ 20240508_tchkVector/test.sv
@@ -1,23 +1,25 @@
 `timescale 1ps/1ps
 module test;
     wire o;
-    reg [1:0] d, clk;
+    reg [2:0] d, clk;
     dut inst(o, clk, d);
     initial begin
-        clk = 2'b00;
-        d = 2'b00;
+        // clk = 3'b111;
+        // d = 3'b111;
         #10;
-        d = 2'b11;
+        d = 3'b011;
         #5;
-        clk = 2'b11;
+        clk = 3'b010;
     end
 endmodule
 
 module dut(o, clk, d);
     output o;
-    input [1:0] clk, d;
+    input [2:0] clk, d;
     reg notifier;
     specify 
-        $setup(posedge d, posedge clk, 10, notifier);
+        $setup(d, clk, 10, notifier);
+        $setup(posedge d, posedge clk, 10, notifier);        
+        $setup(negedge d, negedge clk, 10, notifier);
     endspecify
 endmodule
